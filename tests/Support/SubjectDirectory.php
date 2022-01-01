14b0190436814a628698c132fd927d8ecddd7a78
Jeroen <jgroenendijk@enrise.com>
2022-01-02 00:11:24 +0100

    cs fix

--- tests/Support/SubjectDirectory.php
+++ tests/Support/SubjectDirectory.php
@@ -7,6 +7,8 @@ namespace JeroenG\Autowire\Tests\Support;
 final class SubjectDirectory
 {
     public const ALL = __DIR__.'/Subject';
+
     public const CONTRACTS = __DIR__.'/Subject/Contracts';
+
     public const DOMAIN = __DIR__.'/Subject/Domain';
 }
